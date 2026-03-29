1c573798c611469d7606e95bb27916f48e0f4beb
Michael D <michaeld@Michaels-Mac-mini.local>
2026-03-28 18:19:40 -0700

    feat: add Microsoft Clarity (w33ozv4scw)

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,6 +100,16 @@ export default function RootLayout({
             gtag('config', 'G-0TFMJKX9BK');
           `}
         </Script>
+        {/* Microsoft Clarity */}
+        <Script id="clarity-init" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "w33ozv4scw");
+          `}
+        </Script>
       </head>
       <body
         className={`${geist.variable} font-sans antialiased`}
